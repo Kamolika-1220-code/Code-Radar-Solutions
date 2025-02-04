66629ad6a68bc75def0ffe329a64e8cf11a00596
Kamolika-1220-code <singlakamolika@gmail.com>
2025-02-04 11:24:26 +0530

    Solution to Check Triangle Type

--- Check Triangle Type@@6788dd63490637cfc44e1b97/code.c
+++ Check Triangle Type@@6788dd63490637cfc44e1b97/code.c
@@ -7,7 +7,7 @@ int main()
         printf("Equilateral");
     else if(a==b || b==c || c==a)
         printf("Isosceles");
-    else if(a!=b!=c)
+    else 
         printf("Scalene");        
     return 0;
 }
