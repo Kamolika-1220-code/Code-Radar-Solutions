16d8951d03e0c8a55c6046475855c29022691cbb
Kamolika-1220-code <singlakamolika@gmail.com>
2025-02-28 13:02:27 +0530

    Solution to Simple Calculator

---
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c;
+    c=(+,-,*,/);
+    scanf("%d",&a);
+    scanf("%d",&b);
+    scanf("%d",&c);
+    if(c==+)
+        printf(a+b);
+    return 0;
+}
